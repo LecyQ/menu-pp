4cc3da2ca6fd9f8a3a3fd3b4ec44d7fc2fc1855a
LecyQ <mertyzc99@gmail.com>
2022-09-08 14:57:08 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -22,7 +22,7 @@ Route::get('/', function () {
     $general = DB::table('generals')->where('id',1)->get();
 
 
-    return view('home.index',compact(['category','item','itemId','uniqueCategory','uniqueItemMaincategory','uniqueItemSubcategory']));
+    return view('home.index',compact(['category','item','itemId','uniqueCategory','uniqueItemMaincategory','uniqueItemSubcategory','general']));
 
 
 
